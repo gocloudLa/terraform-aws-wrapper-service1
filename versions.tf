ce8b23aaa14b73768b8eba3d656e184724745b99
Federico Midaglia <fmidaglia@gocloud.la>
2025-07-31 10:23:48 -0300

    feat(versions): update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.0"
+   required_version = ">= 1.6.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 5.31.0"
     }
     random = ">= 2.2.0"
   }
